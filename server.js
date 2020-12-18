afe9340ae840e0675e13e4af7a2b894653c67532
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2020-12-17 22:57:18 -0300

    usando body parser para el body y query de un request

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', (req, res) => {
-    res.send('Hola');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
+
+router.get('/', (req, res) => {
+    console.log(req.query)
+    res.send('Hola desde GET!');
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body.text);
+    res.send('Hola desde POST!');
+});
+
+// app.use('/', (req, res) => {
+//     res.send('Hola');
+// });
 
 app.listen(3010);
 console.log('Escuchando en el puerto 3010');
