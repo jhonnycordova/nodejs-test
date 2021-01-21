5ff3d08e54a90a208f640110105f29b98338d70a
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-01-20 23:14:10 -0300

    db

--- components/message/store.js
+++ components/message/store.js
@@ -1,16 +1,5 @@
 
-const db = require('mongoose');
 const Model = require('./model');
-
-db.Promise = global.Promise;
-
-db.connect("mongodb+srv://jhonnycordova:.Jho2021@cluster0.eu6jb.mongodb.net/backend_node_test_db?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('DB connected'))
-.catch((err) => {
-    console.log(err);
-});
 
 function addMessage(message) {
     const myMessage = new Model(message);
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./db');
 const router = require('./network/routes');
 
+db('mongodb+srv://jhonnycordova:.Jho2021@cluster0.eu6jb.mongodb.net/backend_node_test_db?retryWrites=true&w=majority');
 var app = express();
 
 app.use(bodyParser.json());
