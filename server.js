55b161ba715b94c75009ec6f896f493cd7a0481f
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-02-02 22:17:59 -0300

    añadiendo socket

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').Server(app);
 
+const bodyParser = require('body-parser');
 const db = require('./db');
+const socket = require('./socket');
 const router = require('./network/routes');
 
 db('mongodb+srv://jhonnycordova:.Jho2021@cluster0.eu6jb.mongodb.net/backend_node_test_db?retryWrites=true&w=majority');
-var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(router);
+socket.connect(server);
 
 router(app);
 
@@ -17,5 +19,6 @@ router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3010);
-console.log('Escuchando en el puerto 3010');
+server.listen(3010, function(){
+    console.log('Escuchando en el puerto 3010');
+});
